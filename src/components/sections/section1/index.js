0ad3c7167d0b72f3a91590ee2297a804e0907071
Maksym67 <maxpasternak54@gmail.com>
2022-12-19 00:49:48 +0200

    update

--- src/components/sections/section1/index.js
+++ src/components/sections/section1/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./index.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Section1() {
+    useEffect(() => {
+        AOS.init();
+      }, [])
+
     return (
         <div className="section1">
             <div className="color-section"></div>
