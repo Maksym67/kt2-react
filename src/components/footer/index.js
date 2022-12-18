e097446ecef7d154807b4572330db88beb9f18c7
Maksym67 <maxpasternak54@gmail.com>
2022-12-18 23:26:28 +0200

    update

---
+++ src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="main-footer-block">
+                <span>123</span>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/sections/section1/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css"
+
+function Section1() {
+    return (
+        <div className="section1">
+            <div className="color-section"></div>
+        </div>
+    );
+}
+
+export default Section1;
