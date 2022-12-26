595767b70407d2506b5600e481e1996bcc838adc
Maksym67 <maxpasternak54@gmail.com>
2022-12-26 10:09:45 +0200

    update

--- src/components/sections/home-section/index.js
+++ src/components/sections/home-section/index.js
@@ -100,9 +100,6 @@ export default class Home extends Component {
                                 <img src={Image4}></img>
                             </div>
                         </Slider>
-                        <div className="nav-btns">
-                            <a className="slick-next" href="#">123</a>
-                        </div>
                     </div>
                 </div>
                 <div className="row">
